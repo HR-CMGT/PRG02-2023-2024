e0333e87d63d59cf35d481c911700157b0980572
Martijn Willemse <mewillemse@gmail.com>
2023-12-07 15:36:33 +0100

    Exercise JOIN les7

---
+++ les7/exercises/includes/les7-import.sql
@@ -0,0 +1,23 @@
+INSERT INTO `albums` (`name`, `artist`, `genre`, `year`, `tracks`)
+VALUES ('19', 'Adele', 'Pop', 2008, 12);
+
+INSERT INTO `artists` (`id`, `name`)
+VALUES (1, 'Angels & Airwaves'),
+       (2, 'Dream Theater'),
+       (3, 'Kendrick Lamar'),
+       (4, 'Harrie Jekkers'),
+       (5, 'Beyoncé'),
+       (6, 'Limp Bizkit'),
+       (7, 'Arctic Monkeys'),
+       (8, 'Kygo'),
+       (9, 'Harry Styles'),
+       (10, 'Ed Sheeran'),
+       (11, 'Adele'),
+       (12, 'Queen'),
+       (13, 'The Weeknd'),
+       (14, '30 Seconds To Mars'),
+       (15, 'Dua Lipa'),
+       (16, 'Hardwell'),
+       (17, 'Muse'),
+       (18, 'Billie Eilish'),
+       (19, 'Joe Satriani');
